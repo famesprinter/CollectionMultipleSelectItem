85f7e794a7d0e97302ac14217eeed664c44255bf
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-12-17 06:37:35 +0700

    - Create CollectionView. - Create ViewModel.

--- CollectionMultipleSelect/ViewController.swift
+++ CollectionMultipleSelect/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Variable
+    let viewModel = ViewModel()
 
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+}
+
+// MARK: - UICollectionView Delegate & DataSource
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numberOfItem()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
 }
 
---
+++ CollectionMultipleSelect/ViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ViewModel.swift
+//  CollectionMultipleSelect
+//
+//  Created by Kittitat Rodphotong on 12/17/2559 BE.
+//  Copyright © 2559 DevGo. All rights reserved.
+//
+
+import Foundation
+
+class ViewModel {
+    // MARK: - Funtion
+    func numberOfItem() -> Int {
+        return 15
+    }
+}
