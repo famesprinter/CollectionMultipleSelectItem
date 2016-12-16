36a90311430ee48ed19065893712d089c11fb4ac
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-12-17 06:24:59 +0700

    - Initial Project.

---
+++ CollectionMultipleSelect/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CollectionMultipleSelect
+//
+//  Created by Kittitat Rodphotong on 12/17/2559 BE.
+//  Copyright © 2559 DevGo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
