fa99ea328dae4f62da49130417c9c786068927e2
Kittitat Rodphotong <kittitat_rpt@hotmail.com>
2016-12-17 06:40:31 +0700

    - Add image selection in collection view cell.

---
+++ CollectionMultipleSelect/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  CollectionMultipleSelect
+//
+//  Created by Kittitat Rodphotong on 12/17/2559 BE.
+//  Copyright © 2559 DevGo. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    // MARK: - IBOutlet
+    @IBOutlet weak var imageSelection: UIImageView!
+    
+    
+}
